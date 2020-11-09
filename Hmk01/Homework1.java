e5dc4d5b0f8e86c4ebd7c497ebc078090b37fe66
TheDataNomad <alsarooj94@gmail.com>
2020-11-09 14:09:35 +0100

    Solved Homework 1

--- Hmk01/Homework1.java
+++ Hmk01/Homework1.java
@@ -1,25 +1,25 @@
 import java.util.Scanner;
 import java.io.*;
 
-public class Homework2 {
+public class Homework1 {
     
     private static int[] readFile() {
         try {
             Scanner sc = new Scanner(new File("input.txt"));
             int a = sc.nextInt();
-            int b = sc.nextInt()
+            int b = sc.nextInt();
             return new int[] { a, b };
         } catch (FileNotFoundException ex) {
             return null;
         }
     }
     
-    private static void writeFile(int number) {
+    private static void writeFile(long number) {
         try {
             Writer writer = new BufferedWriter(
                     new OutputStreamWriter(
                         new FileOutputStream("output.txt"), "ascii"));
-            writer.write(new Integer(number).toString();)
+            writer.write(new Long(number).toString());
             writer.close();
           } catch (IOException ex) {
               
@@ -28,7 +28,8 @@ public class Homework2 {
 
     
     public static void main(String[] args) {
-        int[] vals = readFile()
+        int[] vals = readFile();
         long mult = vals[0] * vals[1];
         writeFile(mult);
     }
+}
